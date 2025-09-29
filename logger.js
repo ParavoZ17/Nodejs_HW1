3c7788751371b7d490f24ffb82351a612c404d6d
Mykola Naumenko <1712nn2012@gmail.com>
2025-09-29 09:08:28 +0200

    переробив через проміси

--- logger.js
+++ logger.js
@@ -1,14 +1,9 @@
+import { appendFile } from "node:fs/promises";
 
-import  fs from 'fs';
-
-
-export function logMessage(message) {
-  
-  fs.appendFile('log.txt', message + '\n', (err) => {
-    if (err) {
-      console.error('Помилка при записі лога:', err);
-    }
-  });
+export async function logMessage(message) {
+  try {
+    await appendFile("log.txt", message + "\n");
+  } catch (err) {
+    console.error("Помилка при записі лога:", err);
+  }
 }
-
-
